af10ee0c9e24caf171cec5883bdb48bd82ec87c0
rissdev2 <104282927+rissdev2@users.noreply.github.com>
2022-11-18 17:06:03 +0500

    add counter

--- Components/Dashboard.jsx
+++ Components/Dashboard.jsx
@@ -1,46 +1,25 @@
 import React from 'react';
 
 const Dashboard = () => {
-   
 
-var stripe_load = () => {
-    if (process.browser) {
-        var aScript = document.createElement('script');
-        aScript.type = 'text/javascript';
-        aScript.src = " https://js.stripe.com/v3/";
-
-        document.head.appendChild(aScript);
-        aScript.onload = () => {
-            const counters = document.querySelectorAll('.counter');
-            const speed = 3000; // The lower the slower
-            
-            counters.forEach(counter => {
-                const updateCount = () => {
-                    const target = +counter.getAttribute('data-target');
-                    const count = +counter.innerText;
-            
-                    // Lower inc to slow and higher to slow
-                    const inc = target / speed;
-            
-                    // console.log(inc);
-                    // console.log(count);
-            
-                    // Check if target is reached
-                    if (count < target) {
-                        // Add inc to count and output in counter
-                        counter.innerText = count + inc;
-                        // Call function every ms
-                        setTimeout(updateCount, 1);
-                    } else {
-                        counter.innerText = target;
-                    }
-                };
-            
-                updateCount();  
-            });
-        };
+    
+    function numCounter(tagId,maxCount,speed){
+        var initialNumber = 0;
+        function counter(){
+            document.getElementById(tagId).innerHTML = initialNumber;
+            ++initialNumber;
+        }
+        var counterDelay = setInterval(counter,speed);
+        function totalTime(){
+            clearInterval(counterDelay);
+        }
+        var totalPeriod = speed * (maxCount);  
+        setTimeout(totalTime, totalPeriod);
     }
-};
+    
+    numCounter("Projects",12,80);
+    numCounter("Clients",105,20);
+    numCounter("Partners",310,10);
 
 
 
@@ -55,7 +34,7 @@ var stripe_load = () => {
 
 
           <div className="container">
-            <div className="row dashboard-sec">
+            <div className="row dashboard-sec ">
                 <div className="col-md-7">
                     <h2>
                     Use wealth expert tools without <span className='main-color spn-sty'> coding skills</span>
@@ -72,15 +51,23 @@ var stripe_load = () => {
                     <img src="assets/img/Dashboard.png" className='img-fluid' alt="" />
                 </div>
             </div>
-            {/* <div className="row conter-pts ">
-                <div className="col-md-4 d-flex align-items-center">
-                <div className="counter" data-target="290"><h2>0</h2></div> 
+            <div className="row conter-pts ">
+                <div className="col-md-4 justify-content-center d-flex align-items-center">
+                <h2><span id="Partners">0</span></h2>
                 <h3 className='main-color'>M</h3>
                 <p>Startup we <br /> have Funded</p>
                 </div>
-                <div className="col-md-4"></div>
-                <div className="col-md-4"></div>
-            </div> */}
+                <div className="col-md-4 justify-content-center  d-flex align-items-center">
+                <h2><span id="Projects">0</span></h2>
+                <h3 className='main-color'>K</h3>
+                <p>Startup we <br /> have Funded</p>
+                </div>
+                <div className="col-md-4 justify-content-center  d-flex align-items-center">
+                <h2><span id="Clients">0</span></h2>
+                <h3 className='main-color'>%</h3>
+                <p>Startup we <br /> have Funded</p>
+                </div>
+            </div>
         </div>  
 
         </>
