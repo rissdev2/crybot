3d436aaca44c7799e90d3ae224e19932d9356c9a
rissdev2 <104282927+rissdev2@users.noreply.github.com>
2022-11-22 15:16:40 +0500

    Desktop responsive

--- pages/index.js
+++ pages/index.js
@@ -26,72 +26,71 @@ export default function Home() {
       <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" />
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" />
       <Script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" />
-      
-      <section className="home-sec">
 
-      <Header/>
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <Hero />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <Partners />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <About />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <Cards />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <Dashboard />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <Experience />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <Aisolution />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <br className="br-hide" />
-      <Footer />
+      <section className="home-sec">
+        <Header />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <Hero />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <Partners />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <About />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <Cards />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <Dashboard />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <Experience />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <Aisolution />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <br className="br-hide" />
+        <Footer />
       </section>
     </>
   );
